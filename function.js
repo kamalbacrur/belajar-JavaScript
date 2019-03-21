5855f6683eee7c8724d55b391f318b562c0990be
Muhamad Kamal <kmuhamad361@gmail.com>
2019-03-21 18:55:23 +0700

    tambahkan function.js

---
+++ function.js
@@ -0,0 +1,18 @@
+function jalanpertama() {
+    console.log("ini adalah belajar function");
+}
+
+const jalanKedua = function() {
+    console.log("Ini adalah cara kedua dari function");
+}
+const jalan4= function() {
+    console.log("latihan latihan");
+}
+function jalan3 () {
+    console.log("ada banyak cara buat sukses");
+}
+
+jalanKedua();
+jalanpertama();
+jalan3();
+jalan4();
