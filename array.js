11fe04f5edea4fc7a82e609102fcb1408bdb6cdd
Muhamad Kamal <kmuhamad361@gmail.com>
2019-03-21 23:03:34 +0700

    belajar array

---
+++ array.js
@@ -0,0 +1,19 @@
+function belajarArray () {
+    var data = [1,3,5,7,9,11,13]
+    var nama = ["kamal", "tika", "umi", "ammar"]
+
+    console.log(data.length)
+    console.log(nama.length)
+
+    console.log("keluar nya adalah nama", nama[1])
+}
+belajarArray();
+console.log("=====================================")
+function array () {
+    var anggota = ["kamal", "yazid", "ammar", "rahmat", "matius"]
+    var kerjaan = ["enggineering", "bank", "percetakan", "peternakan", "guru"]
+
+    console.log("namanya adalah", anggota[3]);
+    console.log("kerjaan adalah" , kerjaan[3])
+}
+array();
