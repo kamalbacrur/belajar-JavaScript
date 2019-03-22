292397df663c357acf5de5bbe4d86deb19c4c4da
Muhamad Kamal <kmuhamad361@gmail.com>
2019-03-22 23:29:37 +0700

    belajar array shift dan pop

--- pop-array.js
+++ pop-array.js
@@ -1,7 +1,20 @@
-function array_pop() {
+console.log("============================================")
+console.log("               Menggunakan Pop              ") 
+function array_pop() { 
     var kota = ["jakarta", "bandung", "makassar", "tegal"]
 console.log(kota);
-kota.pop();
+var kota1 = kota.pop();
+console.log(kota1);
 return kota;
 }
 console.log(array_pop());
+console.log("============================================")
+console.log("               Menggunakan Shift            ")
+function array_shift() {
+    var kota = ["jakarta", "bandung", "makassar", "tegal"];
+    console.log(kota);
+    var kota1 = kota.shift()
+    console.log(kota1);
+    return kota;
+}
+console.log(array_shift());
