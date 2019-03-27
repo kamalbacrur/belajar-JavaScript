84879196a7536075aef82ad548e1067a4744f71c
Muhamad Kamal <kmuhamad361@gmail.com>
2019-03-27 13:51:38 +0700

    belajar membuat array-filter

---
+++ array-filter.js
@@ -0,0 +1,29 @@
+console.log("array-filter adalah array yang digunakan untuk memfilter data dalam array sesuai dengan item yang ingin difilter")
+function arrayFilter() {
+    var negara = [{negara : "jepang", benua : "asia"},
+                  {negara : "malaysia", benua : "asia"},
+                  {negara : "inggris", benua : "eropa"},
+                  {negara : "canada" , benua : "amerika"},
+                  {negara : "zimbabwe" , benua : "afrika"}
+
+    ];
+    var benuaAsia = negara.filter(function(item){
+        return item.benua === "asia";
+    });
+    return benuaAsia;
+}
+console.log(arrayFilter())
+console.log("=======================================================")
+function cobalagi() {
+    var kelas = [{nama : "kamal" , kelas : "XI-RPL"},
+                 {nama : "nuratika", kelas : "XI-AK2"},
+                 {nama : "putri silvana" , kelas : "XI-PM2"},
+                 {nama : "yazid amirullah", kelas : "XI-RPL"},
+                 {nama : "fadhil rabbani", kelas : "XI-RPL"}
+    ];
+    var kelasRPL = kelas.filter(function(item){
+        return item.kelas === "XI-RPL";
+    });
+    return kelasRPL;
+}
+console.log(cobalagi())
